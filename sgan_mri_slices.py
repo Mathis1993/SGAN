5e9fd506c63c112c01d311a16010eb77d6291d57
Mathis Erichsen <m_eric01@ivv5net.wwu.de>
2019-11-08 16:01:59 +0100

    structured script by declaring sections

--- sgan_mri_slices.py
+++ sgan_mri_slices.py
@@ -8,30 +8,45 @@ from utils.slicing import slicing
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
 
-#dataframe containing path and name to t1-images per subject and their ages
-q = pd.read_csv("mathis_t1_FOR2107_HC_MDD.csv")
+    ######################
+    ###LOAD IMAGE PATHS###
+    ######################
 
-#make sure to only have cases with HC-diagnosis
-q = q.loc[q["research_diagnosis"] == "HC", :]
+    #dataframe containing path and name to t1-images per subject and their ages
+    q = pd.read_csv("mathis_t1_FOR2107_HC_MDD.csv")
 
-#t1-images paths and names: append full image paths as new column
-scans = list(q.loc[: ,"NeuroRaw_T1default__image_path"] + "/" + q.loc[: ,"NeuroRaw_T1default__image_name"])
-q['scans'] = scans
+    #make sure to only have cases with HC-diagnosis
+    q = q.loc[q["research_diagnosis"] == "HC", :]
 
-#subset with relevant variables
-q_selection = q.loc[:, ["MS_ID", "research_diagnosis", "scans", "age"]]
+    #t1-images paths and names: append full image paths as new column
+    scans = list(q.loc[: ,"NeuroRaw_T1default__image_path"] + "/" + q.loc[: ,"NeuroRaw_T1default__image_name"])
+    q['scans'] = scans
 
-#Before shuffling, get path to an image that has target dimensions, to use as reference when loading the immages
-ref_img = q_selection.loc[q_selection.index[0] , 'scans']
+    #subset with relevant variables
+    q_selection = q.loc[:, ["MS_ID", "research_diagnosis", "scans", "age"]]
 
-#Shuffle df
-q_selection = q_selection.sample(frac=1)
+    #Before shuffling, get path to an image that has target dimensions, to use as reference when loading the immages
+    ref_img = q_selection.loc[q_selection.index[0] , 'scans']
 
-#load 10 images
-imgs, fails = load_and_resample(ref_img, list(q_selection.loc[q_selection.index[0:10], "scans"]))
+    #Shuffle df
+    q_selection = q_selection.sample(frac=1)
 
-#To get horizontal slices, we need to take slices from the 2nd dimension
-#--> Swap axes, so that we have (x, y, slice, img number)
-imgs = np.swapaxes(imgs,1,2)
+    #################
+    ###LOAD IMAGES###
+    #################
 
-imgs_sliced = slicing(imgs=imgs, n_slices=5)
+    #load 10 images
+    imgs, fails = load_and_resample(ref_img, list(q_selection.loc[q_selection.index[0:10], "scans"]))
+
+    ##################
+    ###SLICE IMAGES###
+    ##################
+
+    #To get horizontal slices, we need to take slices from the 2nd dimension
+    #--> Swap axes, so that we have (x, y, slice, img number), i.e. the dimension to be sliced is the third one
+    #(slicing function expects this at the moment)
+    imgs = np.swapaxes(imgs,1,2)
+
+    imgs_sliced, subject_idx = slicing(imgs=imgs, n_slices=5)
+
+    
