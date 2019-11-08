26e8306f47bdefa8d849cafd43307de4c79fa120
Mathis Erichsen <m_eric01@ivv5net.wwu.de>
2019-11-08 15:40:42 +0100

    created utils directory

--- utils/load_and_resample.py
+++ utils/load_and_resample.py
@@ -31,7 +31,8 @@ def load_and_resample(ref_img, imgs):
     Load mri-images iteratively and resampling them to the dimensions of a reference image.
     :param ref_img: (String) Path to reference image having target dimensions
     :param imgs: (List) Paths to all images (including the reference image)
-    :return: Numpy array with all images that could be loaded of shape (target dimensions plus list dimension)
+    :return: - Numpy array with all images that could be loaded of shape (target dimensions plus list dimension)
+             - List of paths for which loading failed
     """
     #load reference image to which dimensions all other images will be resampled
     ref_img = load_img(ref_img)
@@ -41,11 +42,13 @@ def load_and_resample(ref_img, imgs):
     #Find first image that can be loaded
     go_on = True
     i = 0
+    fails = list()
     while(go_on):
         try:
             first_img = load_img(imgs[i])
             go_on = False
         except:
+            fails.append(imgs[i])
             i += 1
 
     #Right now, only able to load one image after another (see read_multiple_niftis.py)
@@ -59,7 +62,6 @@ def load_and_resample(ref_img, imgs):
     #add fourth dimension for concatenation
     img_data = np.expand_dims(img_data, axis=3)
     #Starting the loop from the image coming after the first one that could be loaded
-    fails = i
     succ = 1
     for img in imgs[i+1:]:
         try:
@@ -73,7 +75,45 @@ def load_and_resample(ref_img, imgs):
             img_data = np.concatenate((img_data, cur_img), axis=3)
             succ += 1
         except:
-            fails += 1
+            fails.append(img)
     #How many successful loads, how many fails?
-    print("Successfully loaded {}/{} images, failed to load {}/{} images".format(succ, len(imgs), fails, len(imgs)))
-    return(img_data)
+    print("Successfully loaded {}/{} images, failed to load {}/{} images".format(succ, len(imgs), len(fails), len(imgs)))
+    return(img_data, fails)
+
+imgs, fails = load_and_resample(ref_img, list(q_selection.loc[q_selection.index[0:10], "scans"]))
+
+#To get horizontal slices, we need to take slices from the 2nd dimension
+#--> Swap axes, so that we have (x, y, slice, img number)
+imgs = np.swapaxes(imgs,1,2)
+
+def slicing(n_slices=5, imgs=imgs):
+    """
+    Takes a 4D-np-array of mri-images (x,y,slice_dimension,subjects) and takes n_slices from the third dimension.
+    Returns a 3D-array of (x,y,n_slices*subjects), so n_slices*subjects 2D-images that are ordered like this:
+    The first image is the first slice of the first subject. Then the first slice of the second subject and so on.
+    Which image belongs to which subject is documented in a 1D-array, which is also returned.
+    :param n_slices: How many slices we want. Taken from the middle.
+    :param imgs: 4D-array of images with (x,y,slice_dimension,subjects)
+    :return: - 3D-array of sliced images with (x,y,n_slices*subjects)
+             - 1D-array of subject indices assigning a subject index to each 2D-image slice.
+    """
+    #ToDo: Parametrize the dimension which should be sliced. Idea: Give slice dimension as parameter. Swap axes so that
+    #the slicing dimension is in the third place
+    #take slices from the middle
+    start_index = int(imgs.shape[2]/2) - int(n_slices/2)
+    end_index = start_index + n_slices
+    #slice
+    imgs_sliced = imgs[:, :, start_index:end_index, :]
+    #reshape into 3D-array so that all slices of all subjects are inside the 3rd dimension
+    imgs_sliced = imgs_sliced.reshape(imgs_sliced.shape[0], imgs_sliced.shape[1], -1)
+    #order: first slice of all subjects, then second slice of all subjects and so on
+    #To understand why, see reshape_sliced_image_array.py
+    #create array holding slice-person-mapping: each subject is assigned an index. The list of subjects is repeated
+    #as many times as we have slices
+    n_subjects = imgs.shape[-1]
+    subject_idx = np.array([i for i in range(n_subjects)] * n_slices)
+    return imgs_sliced, subject_idx
+
+
+
+
---
+++ utils/reshape_sliced_image_array.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import warnings
+from load_and_resample import load_and_resample
+from matplotlib import pyplot as plt
+
+#suppress Deprecation Warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+
+#READ DATA
+#dataframe containing path and name to t1-images per subject and their ages
+q = pd.read_csv("mathis_t1_FOR2107_HC_MDD.csv")
+#make sure to only have cases with HC-diagnosis
+q = q.loc[q["research_diagnosis"] == "HC", :]
+#t1-images paths and names: append full image paths as new column
+scans = list(q.loc[: ,"NeuroRaw_T1default__image_path"] + "/" + q.loc[: ,"NeuroRaw_T1default__image_name"])
+ref_img = scans[0]
+
+#load images
+imgs, fails = load_and_resample(ref_img, scans[0:10])
+
+#Get slices
+#To get horizontal slices, we need to take slices from the 2nd dimension
+#--> Swap axes, so that we have (x, y, slice, img number)
+imgs = np.swapaxes(imgs,1,2)
+#amount of slices (taken from the middle)
+n_slices = 5
+start_index = int(imgs.shape[2]/2) - int(n_slices/2)
+end_index = start_index + n_slices
+imgs_sliced = imgs[:, :, start_index:end_index, :]
+#get all slices over all subjects into one dimension
+imgs_sliced = imgs_sliced.reshape(256,176,-1)
+
+#Now we have 5x10=50 slices, but in what oder? Where are slices 1 to 5 from subject 1, and where are 1 to 5 from
+#subject 2 and so on?
+
+#From reshape/ravel documentation:
+#"In row-major, C-style order, in two dimensions, the row index varies the slowest, and the column index the quickest.
+#This can be generalized to multiple dimensions, where row-major order implies that the index along the first axis
+#varies slowest, and the index along the last quickest."
+# --> When reshaping (256, 176, 5, 10) (5 slices of 256x176 over 10 subjects),  to (256, 176, 50), the subject-index
+#will vary quickest, so that the first ten entries of the third dimension in (256, 176, 50) will be the first slice
+#for each of the ten subjects, the second ten entries will be the second slice for each of the ten subjects and so on
+
+#Confirmation:
+#slice 126 from the first subject (after swapping dimensions from (256,256,176) to (256,176,256)
+plt.clf()
+plt.imshow(imgs[:,:,126,0])
+plt.savefig("after_swapping.png")
+#slice 126 from the first subject after resizing the array so that all slices over all subjects are in one dimension
+plt.clf()
+plt.imshow(imgs_sliced.reshape(256,176,-1)[:,:,0])
+plt.savefig("after_reslicing.png")
+
+#slice 126 from the second subject (after swapping dimensions from (256,256,176) to (256,176,256)
+plt.clf()
+plt.imshow(imgs[:,:,126,1])
+plt.savefig("after_swapping2.png")
+#slice 126 from the second subject after resizing the array so that all slices over all subjects are in one dimension
+plt.clf()
+plt.imshow(imgs_sliced.reshape(256,176,-1)[:,:,1])
+plt.savefig("after_reslicing2.png")
+
+#after_swapping and after_reslicing as well as after_swapping2 and after_reslicing2 should be totally identical!
+
+#If we would swap the subject and slice axes, would we get all 5 slices of subject 1 for the first five entries, then
+#all 5 slices of subject two for the second 5 entries and so on?
+
